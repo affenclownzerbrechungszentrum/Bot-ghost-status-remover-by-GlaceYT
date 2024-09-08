8a21f0740534ee9b9cd6fff1846ea45ab5f75cb0
affenclownzerbrechungszentrum <xpersonprivaternutzer@gmail.com>
2024-09-08 13:04:55 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["by RobinIstKuhl","Playing Minecraft"];
 
 
 let currentIndex = 0;
